c41a85e6407d58c2523377b4abcbc3a3299bc71f
Monika <monidavies@gmail.com>
2023-01-18 15:41:20 -0800

    Adds basic site

--- food/views.py
+++ food/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Food
+from .serializer import FoodSerializer
+
 
 # Create your views here.
+class FoodList(generics.ListCreateAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+
+class FoodDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
